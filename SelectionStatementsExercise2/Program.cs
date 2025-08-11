e0bcb2385d3aa8cbe888676dabb587d14a643db2
Eduardo Mata <eddiemata15533@gmail.com>
2025-08-10 20:28:41 -0500

    finish switch case exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -2,9 +2,35 @@
 {
     public class Program
     {
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject? ");
+            string Subject = Console.ReadLine();
+
+            switch (Subject.ToLower())
+            {
+                case "art":
+                    Console.WriteLine("Why did the painter go to jail? Because they were framed..");
+                    break;
+                case "music":
+                    Console.WriteLine("What kind of music are balloons afraid of?...Pop music.");
+                    break;
+                case "math":
+                    Console.WriteLine("How dose a mathematician plow fields?...With a pro-tractor.");
+                    break;
+                case "science":
+                    Console.WriteLine("What did one ion say to another ion?...I've got my ion you.");
+                    break;
+                
+                default:
+                    Console.WriteLine("That's a great subject ");
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+            
         }
     }
 }
